6b0c93ece16d63c57276b73001571bbfbff85188
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2018-10-04 11:37:59 -0500

    :sparkles: Mailtrap support.

---
+++ SternerStuffWordPress/Mailtrap.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SternerStuffWordPress;
+
+class Mailtrap {
+	function __construct() {
+		add_action('phpmailer_init', [$this, 'enable_mailtrap']);
+	}
+
+	public function enable_mailtrap($phpmailer) {
+		if(!getenv('MAILTRAP_USER') || !getenv('MAILTRAP_PASSWORD') || getenv('WP_ENV') != 'development') {
+			return;
+		}
+		$phpmailer->isSMTP();
+		$phpmailer->Host = 'smtp.mailtrap.io';
+		$phpmailer->SMTPAuth = true;
+		$phpmailer->Port = 2525;
+		$phpmailer->Username = getenv('MAILTRAP_USER');
+		$phpmailer->Password = getenv('MAILTRAP_PASSWORD');
+	}
+}
--- sterner-stuff-wordpress-core.php
+++ sterner-stuff-wordpress-core.php
@@ -3,7 +3,7 @@
 Plugin Name: Sterner Stuff WordPress Core
 Plugin URI: https://sternerstuffdesign.com
 Description: Baseline settings for Sterner Stuff WordPress sites
-Version: 6.0.0
+Version: 6.1.0
 Author: Ethan Clevenger
 Author URI: https://sternerstuffdesign.com
 */
@@ -13,21 +13,23 @@ class SternerStuff {
 
 	function __construct() {
 		/* Autoloader */
-    //See https://github.com/afragen/autoloader/blob/master/plugin.php
-    //Plugin namespace root
-    $root = [
-      'SternerStuffWordPress' => __DIR__.'/SternerStuffWordPress'
-    ];
-    $extra_classes = [];
-    //Load autoloader
-    require_once(__DIR__.'/SternerStuffWordPress/Autoloader.php');
-    $loader = 'SternerStuffWordPress\\Autoloader';
-    new $loader($root, $extra_classes);
+		//See https://github.com/afragen/autoloader/blob/master/plugin.php
+		//Plugin namespace root
+		$root = [
+			'SternerStuffWordPress' => __DIR__.'/SternerStuffWordPress'
+		];
+		$extra_classes = [];
+		//Load autoloader
+		require_once(__DIR__.'/SternerStuffWordPress/Autoloader.php');
+		$loader = 'SternerStuffWordPress\\Autoloader';
+		new $loader($root, $extra_classes);
 
 		//Editing Experience
 		new SternerStuffWordPress\EditingExperience;
 		//Permissions
 		new SternerStuffWordPress\Permissions;
+
+		new SternerStuffWordPress\Mailtrap;
 	}
 
 	//Keep this method at the bottom of the class
