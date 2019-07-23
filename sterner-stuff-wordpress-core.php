5aedc7c3ec96996fbb2fc931b90fe012beefc9d0
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2019-07-23 12:33:03 -0700

    Support Cloudflare whitelist.

--- sterner-stuff-wordpress-core.php
+++ sterner-stuff-wordpress-core.php
@@ -3,12 +3,15 @@
 Plugin Name: Sterner Stuff WordPress Core
 Plugin URI: https://sternerstuffdesign.com
 Description: Baseline settings for Sterner Stuff WordPress sites
-Version: 7.1.2
+Version: 7.2.0
 Author: Ethan Clevenger
 Author URI: https://sternerstuffdesign.com
 */
 
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
 class SternerStuffWordPress {
+
 	private static $self = false;
 
 	function __construct() {
@@ -24,7 +27,20 @@ class SternerStuffWordPress {
 		new SternerStuffWordPress\LimitRevisions();
 
 		SternerStuffWordPress\Constants::define();
-		
+
+		if( is_plugin_active( 'wp-fail2ban/wp-fail2ban.php' ) ) {
+			$this->whitelistActiveProxies();
+		}		
+	}
+
+	public function whitelistActiveProxies() {
+		/**
+		 * Only one proxy whitelist should be defined at a time
+		 * Otherwise you'll get constant re-defined warnings
+		 */
+		if( env('CLOUDFLARE_ENABLED') ) {
+			new SternerStuffWordPress\CloudflareIps;
+		}
 	}
 
 	//Keep this method at the bottom of the class
