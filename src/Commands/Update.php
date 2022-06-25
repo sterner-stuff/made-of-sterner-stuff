705d89b65cc6d3ccbaad7c9a3e38f1b1f7953cb3
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2022-06-24 17:51:47 -0700

    Works.

--- src/Commands/Update.php
+++ src/Commands/Update.php
@@ -8,6 +8,8 @@ class Update extends Command {
 
 	public $name = 'update';
 
+	private $updateInfo = [];
+
 	/**
 	 * Update dependencies and spit out a table.
 	 */
@@ -39,7 +41,7 @@ class Update extends Command {
 
 		$this->updateInfo = array_merge($this->updateInfo, $this->diffPlugins() ?: []);
 		$this->updateInfo = array_merge($this->updateInfo, $this->diffThemes() ?: []);
-		dd($this->updateInfo);
+
 		\WP_CLI\Utils\format_items('table', $this->updateInfo, ['Item', 'Old Version', 'New Version']);
 
 	}
@@ -49,27 +51,51 @@ class Update extends Command {
 	}
 
 	private function updateThemes() {
-		return WP_CLI::runcommand('theme update --all');
+		if (WP_CLI::runcommand('theme list --update=available', [
+			'return' => true,
+		])) {
+			return WP_CLI::runcommand('theme update --all', [
+				'return' => true,
+				'exit_error' => false,
+			]);
+		} else {
+			WP_CLI::log('No themes to update.');
+			return;
+		}
 	}
 
 	private function updatePlugins() {
-		return WP_CLI::runcommand('plugin update --all');
+		if (WP_CLI::runcommand('plugin list --update=available', [
+			'return' => true,
+		])) {
+			return WP_CLI::runcommand('plugin update --all', [
+				'return' => true,
+				'exit_error' => false,
+			]);
+		} else {
+			WP_CLI::log('No plugins to update.');
+			return;
+		}
 	}
 
 	private function getPlugins() {
-		return WP_CLI::runcommand('plugin list --format=json', [
+		return WP_CLI::runcommand('plugin list --fields=name,status,update,version,title --format=json', [
 			'parse' => 'json',
+			'return' => true,
 		]);
 	}
 
 	private function getThemes() {
-		return WP_CLI::runcommand('theme list --format=json', [
+		return WP_CLI::runcommand('theme list --fields=name,status,update,version,title --format=json', [
 			'parse' => 'json',
+			'return' => true,
 		]);
 	}
 
 	private function getCore() {
-		return WP_CLI::runcommand('core version');
+		return WP_CLI::runcommand('core version', [
+			'return' => true,
+		]);
 	}
 
 	private function diffPlugins() {
@@ -93,40 +119,42 @@ class Update extends Command {
 	private function diff($oldThings, $newThings, $type = false) {
 		$updateInfo = [];
 		$installed = [];
+
 		foreach ($newThings as $new) {
-			$installed[$new->name] = $new->version;
+			$installed[$new['name']] = $new['version'];
 			$found = false;
 			foreach ($oldThings as $old) {
-				if ($old->name == $new->name) {
+				if ($old['name'] == $new['name']) {
 					$found = true;
-					if ($old->version == $new->version) {
+					if ($old['version'] == $new['version']) {
 						break;
 					}
 					$updateInfo[] = [
-						$old->name . ($type ? ' [' . $type . ']' : ''),
-						$old->version,
-						$new->version,
+						'Item' => $old['title'] . ($type ? ' [' . $type . ']' : ''),
+						'Old Version' => $old['version'],
+						'New Version' => $new['version'],
 					];
 					break;
 				}
 			}
 			if (!$found) {
 				$updateInfo[] = [
-					$new->name . ($type ? ' [' . $type . ']' : ''),
-					'installed',
-					$new->version,
+					'Item' => $new['title'] . ($type ? ' [' . $type . ']' : ''),
+					'Old Version' => 'installed',
+					'New Version' => $new['version'],
 				];
 			}
 		}
 		foreach ($oldThings as $old) {
-			if (!array_key_exists($old->name, $installed)) {
+			if (!array_key_exists($old['name'], $installed)) {
 				$updateInfo[] = [
-					$old->name . ($type ? ' [' . $type . ']' : ''),
-					'removed',
-					'N/A',
+					'Item' => $old['title'] . ($type ? ' [' . $type . ']' : ''),
+					'Old Version' => 'removed',
+					'New Version' => 'N/A',
 				];
 			}
 		}
+
 		return $updateInfo;
 	}
 
