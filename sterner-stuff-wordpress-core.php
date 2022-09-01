6246825ffaee800ac53b02a4abf93716246cbe4b
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2022-09-01 15:31:48 -0700

    Should not install dev on deploy.

--- sterner-stuff-wordpress-core.php
+++ sterner-stuff-wordpress-core.php
@@ -3,7 +3,7 @@
 Plugin Name: Made of Sterner Stuff
 Plugin URI: https://sternerstuff.dev
 Description: Core functionality for built-to-last Sterner Stuff WordPress sites.
-Version: 11.0.2
+Version: 11.0.3
 Author: Ethan Clevenger
 Author URI: https://sternerstuff.dev
 */
