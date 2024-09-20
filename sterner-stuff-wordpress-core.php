733701c34f92a105921f6f83d86dbafdcd89c9ba
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2024-09-19 22:07:48 -0700

    Bump version

--- sterner-stuff-wordpress-core.php
+++ sterner-stuff-wordpress-core.php
@@ -3,7 +3,7 @@
 Plugin Name: Made of Sterner Stuff
 Plugin URI: https://sternerstuff.dev
 Description: Core functionality for built-to-last Sterner Stuff WordPress sites.
-Version: 11.4.2
+Version: 11.5.0
 Author: Ethan Clevenger
 Author URI: https://sternerstuff.dev
 */
